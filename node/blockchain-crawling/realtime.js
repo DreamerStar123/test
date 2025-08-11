8aec67863aba3c3d85b28125360713ac06457b63
dreamerstar123 <hpeak712@gmail.com>
2025-08-11 11:29:24 -0400

    feat: add blockchain crawling functionality with real-time log monitoring

---
+++ node/blockchain-crawling/realtime.js
@@ -0,0 +1,29 @@
+import { ethers } from "ethers";
+import "dotenv/config";
+
+const provider = new ethers.JsonRpcProvider(
+  `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+);
+
+// Example: USDC token contract
+const contractAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const transferTopic =
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
+provider.on(
+  {
+    address: contractAddress,
+    topics: [transferTopic],
+  },
+  (log) => {
+    const from = "0x" + log.topics[1].slice(26); // last 40 hex chars = address
+    const to = "0x" + log.topics[2].slice(26);
+
+    const [value] = ethers.AbiCoder.defaultAbiCoder().decode(
+      ["uint256"],
+      log.data
+    );
+
+    console.log({ from, to, value: value.toString() });
+  }
+);
